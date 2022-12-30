7b60433f97a88c5ff8a7515bee1cd36d4e20a19c
Rohan das <110011068+ROHANDAS2003@users.noreply.github.com>
2022-12-30 17:22:57 +0530

    Update Granny.py
    Added a function to wish

--- Granny.py
+++ Granny.py
@@ -16,4 +16,28 @@ def speak(audio):
     engine.runAndWait()
 
 
+def wish_me():
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+    global wish
+    if 12 > hour >= 4:
+        wish = "good morning"
+        print("good morning")
+        speak(wish)
+    elif 17 > hour >= 12:
+        wish = "good afternoon"
+        print("good afternoon")
+        speak(wish)
+    elif 21 > hour >= 17:
+        wish = "good evening"
+        print("good evening")
+        speak(wish)
+    elif 4 > hour >= 21:
+        wish = "good night"
+        print("good night")
+        speak(wish)
+
+
 if __name__ == "__main__":
+    wish_me()
+    speak("I am your Virtual granny, You can call me granny")
